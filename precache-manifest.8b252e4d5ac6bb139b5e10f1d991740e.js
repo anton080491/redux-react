1f14060904ce17389b5aee9a6caa6e45e2f54db7
anton <antonirklii@gmail.com>
2021-08-20 15:23:59 +0300

    Updates

---
+++ precache-manifest.8b252e4d5ac6bb139b5e10f1d991740e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec1f1a98a6a0a2104eea46e4ff3567d",
+    "url": "/redux-react/index.html"
+  },
+  {
+    "revision": "9ff250b435c26993c2a6",
+    "url": "/redux-react/static/css/main.81197c56.chunk.css"
+  },
+  {
+    "revision": "887bad6de145edc28ef4",
+    "url": "/redux-react/static/js/2.f737a03e.chunk.js"
+  },
+  {
+    "revision": "9ff250b435c26993c2a6",
+    "url": "/redux-react/static/js/main.d463cc1e.chunk.js"
+  },
+  {
+    "revision": "55425bdc8cf575aab041",
+    "url": "/redux-react/static/js/runtime~main.14aaa5c5.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/redux-react/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/redux-react/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
